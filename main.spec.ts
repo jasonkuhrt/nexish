b692e9dc879ba5b954ad4d573da1d7862415c3ea
Jason Kuhrt <jasonkuhrt@me.com>
2019-10-06 20:10:21 -0400

    tests: cover non-shorthand field scalars

--- main.spec.ts
+++ main.spec.ts
@@ -1,7 +1,20 @@
 import { object } from './main'
 
-describe('defining object types', () => {
-  it('of built in scalars', () => {
+describe('defining blocks', () => {
+  it('is possible to define scalar fields', () => {
+    const Foo = object({
+      name: 'Foo',
+      definition: t => {
+        t.field('boolean', { type: 'Boolean' })
+        t.field('string', { type: 'String' })
+        t.field('id', { type: 'ID' })
+        t.field('int', { type: 'Int' })
+      },
+    })
+    expect(Foo.toConfig()).toMatchSnapshot()
+  })
+
+  it('is possible to use shorthand for scalars', () => {
     const Foo = object({
       name: 'Foo',
       definition: t => {
